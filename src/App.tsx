b5b065c178d77d1dcad405ba0bc42872da76011b
Ahmet Emre Parmaksiz <ahmetemreparmaksiz@gmail.com>
2023-06-09 22:20:25 +0300

    feat: secure paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Header from './components/Header';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import Index from './screens/Index';
+
+function ProtectedIndex() {
+  const user = "eşşek"; // Burada gerçek bir kimlik doğrulama mekanizması olmalı
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
+  return user ? <Index /> : null;
+}
 
 function App() {
   return (
@@ -10,7 +24,9 @@ function App() {
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<RegisterScreen />} />
+          <Route path="/register" element={<RegisterScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/" element={<ProtectedIndex />} />
         </Routes>
       </main>
     </Router>
