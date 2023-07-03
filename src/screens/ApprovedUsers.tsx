387cf127a903376c2651e2dbc7bc9d43c11938dd
ilgimozturk <ilgimozturk@gmail.com>
2023-07-03 09:47:20 +0200

    update public/index.html

--- src/screens/ApprovedUsers.tsx
+++ src/screens/ApprovedUsers.tsx
@@ -14,6 +14,7 @@ import { selectToken } from '../redux/store/userSlice';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVenus, faMars, faEye } from '@fortawesome/free-solid-svg-icons';
 
+
 const phoneNumberFixer = (phoneNumber: string) => {
   const fixedPhoneNumber = phoneNumber.slice(0, 13);
   return fixedPhoneNumber;
