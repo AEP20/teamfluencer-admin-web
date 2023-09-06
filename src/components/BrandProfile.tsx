99fafdaabe29e830d591f2398dc65704d75e38ae
ilgimozturk <ilgimozturk@gmail.com>
2023-09-06 12:55:07 +0200

    .

--- src/components/BrandProfile.tsx
+++ src/components/BrandProfile.tsx
@@ -2,25 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { MoneyExchanges, BillingAddress, BrandType, InfoType } from '../types/brandData';
 import './styles/styles.css';
 import { useSelector } from 'react-redux';
-import { TAaddBalance, TAdeleteNote, TAupdateBrandNote } from '../services/brandAPI';
+import { selectToken } from '../redux/store/userSlice';
+import { TAdeleteNote, TAupdateBrandNote } from '../services/brandAPI';
 
 const BrandProfile = ({ _id, email, phone, country, first_name, last_name, currency, language, job_title, brand_name, balance, billing_address, money_exchanges, notes }: BrandType) => {
   const token = useSelector(selectToken);
-
-  const [_id, setId] = useState('');
-  const [balance, setBalance] = useState(0);
-  const [email, setEmail] = useState('');
-  const [brandName, setBrandName] = useState('');
-  const [country, setCountry] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [currency, setCurrency] = useState('');
-  const [language, setLanguage] = useState('');
-  const [jobTitle, setJobTitle] = useState('');
-  const [notes, setNotes] = useState('');
-  const [moneyExchanges, setMoneyExchanges] = useState<MoneyExchanges[]>([]);
-  const [billingAddress, setBillingAddress] = useState<BillingAddress>({
+  
+  const [brandNotes, setNotes] = useState(notes ?? '');
+  const [moneyExchanges, setMoneyExchanges] = useState<MoneyExchanges[]>(money_exchanges.slice(0,50) ?? []);
+  const [billingAddress, setBillingAddress] = useState<BillingAddress>(billing_address ?? {
     type: '',
     firm_name: '',
     contact_name: '',
@@ -30,8 +20,6 @@ const BrandProfile = ({ _id, email, phone, country, first_name, last_name, curre
     address: '',
     zip_code: '',
   });
-  const [editor, setEditor] = useState(false);
-  const [notification, setNotification] = useState('');
 
   const brandInfo: InfoType[] = [
     { key: 'Brand Name', value: brand_name },
@@ -42,7 +30,9 @@ const BrandProfile = ({ _id, email, phone, country, first_name, last_name, curre
     { key: 'Last Name', value: last_name },
     { key: 'Currency', value: currency },
     { key: 'Language', value: language },
-    { key: 'Job Title', value: jobTitle },
+    { key: 'Job Title', value: job_title },
+    { key: 'Balance', value: balance },
+    { key: 'Brand Id', value: _id },
   ];
 
   const handleUpdateNote = async () => {
@@ -63,35 +53,59 @@ const BrandProfile = ({ _id, email, phone, country, first_name, last_name, curre
     }
   };
 
-  const changeBalance = (id: any, phone: any, token: any) => {
-    TAaddBalance(id, phone, token);
-    setNotification('Phone Number Changed (refresh page)');
-  };
-
-  useEffect(() => {
-    if (notification) {
-      const notificationTimeout = setTimeout(() => {
-        setNotification('');
-      }, 5000);
-
-      return () => clearTimeout(notificationTimeout);
-    }
-  }, [notification]);
-
-  const handleChangeBalance = (e: any) => {
-    setBalance(e.target.value);
-  };
-
   return (
-    <>
-      <div className="profile-section bg-white p-3 shadow-md mb-3 w-3/5">
-        {notification && (
-          <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-3">
-            <span className="flex items-center block sm:inline">{notification}</span>
-          </div>
-        )}
-        <h3 className="section-title text-xl font-bold mb-3">Brand Information</h3>
-        <table className="table-responsive">
+    <div className="profile-section bg-white p-3 shadow-md mb-3 w-3/5">
+      <h3 className="section-title text-xl font-bold mb-3">Brand Information</h3>
+      <table className="table-responsive">
+        <tbody>
+          {brandInfo.map((info, index) => (
+            <tr key={index}>
+              <td className="font-bold text-md">{info.key}</td>
+              <td className="font-semibold">=</td>
+              <td className="font-semibold">{info.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="flex flex-col">
+        <label className="text-sm font-semibold text-gray-500">Note</label>
+        <textarea
+          className="border border-gray-300 rounded-md p-2"
+          value={brandNotes}
+          onChange={(e) => setNotes(e.target.value)}
+        />
+        <div className="flex gap-12 mt-2">
+          <button className="bg-blue-500 text-white rounded-md px-3 py-2 w-full" onClick={handleUpdateNote}>Update Note</button>
+          <button className="bg-blue-500 text-white rounded-md px-3 py-2 w-full" onClick={handleDeleteNote}>Delete Note</button>
+        </div>
+      </div>
+      {Object.values(billingAddress).some((value) => value) && (
+        // Only render this section if there's any value in billingAddress
+        <div className="mt-5">
+          <h3 className="section-title text-lg font-semibold mb-3">Billing Information</h3>
+          <table className="flex">
+            <tbody>
+              <tr className="flex w-full flex-col ml-3 info-value">
+                {Object.entries(billingAddress).map(([key, value]) => (
+                  <td key={key} className="font-bold">
+                    {key.charAt(0).toUpperCase() + key.slice(1)}: <span className="font-semibold">{value}</span>
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
+      {moneyExchanges.length > 0 && (
+        <table className="table-auto mt-10">
+          <thead>
+            <tr>
+              <th className="px-4 py-2 text-xs text-gray-500">Operation</th>
+              <th className="px-4 py-2 text-xs text-gray-500">Amount</th>
+              <th className="px-4 py-2 text-xs text-gray-500">Application ID</th>
+              <th className="px-4 py-2 text-xs text-gray-500">Action Time</th>
+            </tr>
+          </thead>
           <tbody>
             {moneyExchanges.map((exchange, index) => (
               <tr key={index} className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
@@ -103,136 +117,8 @@ const BrandProfile = ({ _id, email, phone, country, first_name, last_name, curre
             ))}
           </tbody>
         </table>
-        <table className="table-responsive">
-          <tbody>
-            <tr>
-              <td className="font-bold text-md">Balance</td>
-              <td className="font-semibold">=</td>
-              <td className="font-semibold">{balance}</td>
-              <button
-                className="text-indigo-600 hover:text-indigo-900 pt-1 pb-1 ml-28"
-                onClick={() => setEditor(!editor)}
-              >
-                Add Balance
-              </button>
-              {editor && (
-                <div className="flex flex-col items-center space-y-2 pb-2">
-                  <div className="flex flex-row space-x-1">
-                    <input
-                      id="balance"
-                      type="integer"
-                      placeholder="(ex: 700)"
-                      value={balance}
-                      className="form-input text-sm border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 w-60 pt-1 pb-1"
-                      onChange={handleChangeBalance}
-                    />
-                    <button
-                      className="text-sm font-medium text-indigo-600 hover:text-indigo-900"
-                      onClick={() => changeBalance(_id, balance, token)}
-                    >
-                      Change
-                    </button>
-                  </div>
-                </div>
-              )}
-            </tr>
-          </tbody>
-        </table>
-        <div className="flex flex-col">
-          <div className="flex flex-col">
-            <label className="text-sm font-semibold text-gray-500">Note</label>
-            <textarea
-              className="border border-gray-300 rounded-md p-2"
-              value={notes}
-              onChange={(e) => setNotes(e.target.value)}
-            />
-          </div>
-          <div className="flex gap-12">
-            <button
-              className="bg-blue-500 text-white rounded-md px-3 py-2 mt-2 w-full"
-              onClick={() => UpdateNote(_id, notes, token)}
-            >
-              Update Note
-            </button>
-            <button
-              className="bg-blue-500 text-white rounded-md px-3 py-2 mt-2 w-full"
-              onClick={() => DeleteNote(_id, token)}
-            >
-              Delete Note
-            </button>
-          </div>
-        </div>
-        {(billingAddress.address ||
-          billingAddress.city ||
-          billingAddress.contactName ||
-          billingAddress.country ||
-          billingAddress.firm_name ||
-          billingAddress.id ||
-          billingAddress.type ||
-          billingAddress.zipCode) && (
-          <>
-            <h3 className="section-title text-lg font-semibold mb-3 mt-5">Billing Information</h3>
-            <tr className="flex">
-              <td className="flex w-full flex-col ml-3 info-value">
-                <tr className="font-bold">
-                  Type : <span className="font-semibold">{billingAddress.type}</span>
-                </tr>
-                <div className="border-t border-gray-200 my-2" />
-                <tr className="font-bold">
-                  Firm Name : <span className="font-semibold">{billingAddress.firm_name}</span>
-                </tr>
-                <div className="border-t border-gray-200 my-2" />
-                <tr className="font-bold">
-                  Contact Name : <span className="font-semibold">{billingAddress.contactName}</span>
-                </tr>
-                <div className="border-t border-gray-200 my-2" />
-                <tr className="font-bold">
-                  ID : <span className="font-semibold">{billingAddress.id}</span>
-                </tr>
-                <div className="border-t border-gray-200 my-2" />
-                <tr className="font-bold">
-                  City : <span className="font-semibold">{billingAddress.city}</span>
-                </tr>
-                <div className="border-t border-gray-200 my-2" />
-                <tr className="font-bold">
-                  Country : <span className="font-semibold">{billingAddress.country}</span>
-                </tr>
-                <div className="border-t border-gray-200 my-2" />
-                <tr className="font-bold">
-                  Address : <span className="font-semibold">{billingAddress.address}</span>
-                </tr>
-                <div className="border-t border-gray-200 my-2" />
-                <tr className="font-bold">
-                  Zip Code : <span className="font-semibold">{billingAddress.zipCode}</span>
-                </tr>
-              </td>
-            </tr>{' '}
-          </>
-        )}
-        {moneyExchanges.length > 0 && (
-          <table className="table-auto mt-10">
-            <thead>
-              <tr>
-                <th className="px-4 py-2 text-xs text-gray-500">Operation</th>
-                <th className="px-4 py-2 text-xs text-gray-500">Amount</th>
-                <th className="px-4 py-2 text-xs text-gray-500">Application ID</th>
-                <th className="px-4 py-2 text-xs text-gray-500">Action Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              {moneyExchanges.map((exchange: MoneyExchanges, index: number) => (
-                <tr key={index} className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
-                  <td className="px-4 py-2 text-sm">{exchange.operation}</td>
-                  <td className="px-4 py-2 text-sm">{exchange.amount}</td>
-                  <td className="px-4 py-2 text-sm">{exchange.application_id}</td>
-                  <td className="px-4 py-2 text-sm">{new Date(exchange.action_time).toLocaleString()}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>
-    </>
+      )}
+    </div>
   );
 };
 
