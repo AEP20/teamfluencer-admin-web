611f2669d0b9e309535e68fc75bc5ecd6960d1a6
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-09-07 12:44:08 +0300

    ref: birkaç test daha eklendi

--- src/services/testAPI.tsx
+++ src/services/testAPI.tsx
@@ -6,8 +6,10 @@ const apiClient = {
   },
 };
 
-export const TAbrandLogin = async (email: any, password: any) => {
-    console.log("içerdee")
+const email = 'ogokcekoca1@gmail.com';
+const password = '123456789';
+
+export const TAbrandLogin = async () => {
   try {
     const response = await fetch(`${apiClient.baseURL}/brand/auth/login`, {
       method: 'POST',
@@ -17,7 +19,7 @@ export const TAbrandLogin = async (email: any, password: any) => {
     });
 
     if (response.status === 200) {
-      return response
+      return response;
     } else {
       throw new Error('Login failed');
     }
@@ -26,9 +28,7 @@ export const TAbrandLogin = async (email: any, password: any) => {
   }
 };
 
-export const TAbrandEmailPassword = async (email: any) => {
-  console.log('içerdee');
-
+export const TAbrandEmailPassword = async () => {
   try {
     const response = await fetch(`${apiClient.baseURL}/brand/auth/passwordEmail`, {
       method: 'POST',
@@ -36,9 +36,56 @@ export const TAbrandEmailPassword = async (email: any) => {
       headers: apiClient.headers,
       body: JSON.stringify({ email }),
     });
-
     if (response.status === 200) {
-      return response
+      return response;
+    } else {
+      throw new Error('Login failed');
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const TAuserAuth = async () => {
+  const phone = '+905516321224';
+  const firebase_id = 'wGgYYpfPSjPB2jV1IkkBhQr6dZD3';
+  const language = 'tr';
+  try {
+    const response = await fetch(`${apiClient.baseURL}/user/auth`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ phone, firebase_id, language }),
+    });
+    const data = await response.json();
+    const token = data.token;
+    if (response.status === 200) {
+      return token;
+    } else {
+      throw new Error('Login failed');
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const TAuserEngagementRate = async (token: any) => {
+  const user_id = '5f92c6dbdff25a7f3dccd175';
+  try {
+    const response = await fetch(`${apiClient.baseURL}/user/engagement`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ user_id }),
+    });
+    const data = await response.json();
+    if (response.status === 200) {
+      return data;
     } else {
       throw new Error('Login failed');
     }
